a0bb557c73782acdc62f032fcacbfcfd78142bcb
Konstantin Welke <kosta@Konstantin-Welkes-MacBook-3.local>
2011-03-02 21:35:27 +0100

    adding units

--- lister40.js
+++ lister40.js
@@ -5,11 +5,27 @@ lister40 = (function() {
 
   o.addArmy = function(name, army) {
     o.armies[name] = army;
+    var units = {}, i, n = army.units.length;
+    for(i = 0; i < n; ++i) {
+      units[army.units[i].name] = army.units[i];
+    }
+    army.units = units;
     $('#armyselect').append('<option>'+name+'</option>');
   }
   
+  o.nextId = function() {
+    //if o or list are not defined, we're screwed anyway :)
+    return o.list._nextid++;
+  }
+  
   o.addUnit = function(name) {
-    alert('adding ' + name);
+    var unit = {
+      id: o.nextId(),
+      name: name
+    }, tmpl = o.army.units[name];
+    
+    o.list.units.push(unit);
+    $('#list').append('<li id="unit-'+unit.id+'">' + tmpl.type + ' - ' + name + '</li>');
   }
 
   //event handler  
@@ -18,7 +34,7 @@ lister40 = (function() {
       var name = $('#armyselect').val(),
         i, sel = $('#newunitselect');
       o.army = o.armies[name];
-      o.list = {};
+      o.list = {_nextid: 0, units: []};
       sel.html('');
       if (!o.army) return;
       
