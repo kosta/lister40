fb653fea57c605ea98142d42d82688972f52fd2b
Konstantin Welke <kosta@Konstantin-Welkes-MacBook-3.local>
2011-03-02 21:12:25 +0100

    add units selector

--- lister40.js
+++ lister40.js
@@ -7,14 +7,39 @@ lister40 = (function() {
     o.armies[name] = army;
     $('#armyselect').append('<option>'+name+'</option>');
   }
+  
+  o.addUnit = function(name) {
+    alert('adding ' + name);
+  }
 
   //event handler  
   $(function() {
-    $('#newunitselect').change(function() {
-      
-    });
     $('#armyselect').change(function() {
-      alert('selected: ' + $('#armyselect').val());
+      var name = $('#armyselect').val(),
+        i, sel = $('#newunitselect');
+      o.army = o.armies[name];
+      sel.html('');
+      if (!o.army) return;
+      
+      for(i in o.army.units) {
+        var unit = o.army.units[i];
+        sel.append('<option>' + unit.type + ' - ' + unit.name + '</option>');
+      }
+    });
+    
+    $('#newunitselect').change(function() {
+      o.addUnit($('#newunitselect').val().split(' - ')[1]);  
+    });
+    $('#newunitform').submit(function() { 
+      try {
+        o.addUnit($('#newunitselect').val().split(' - ')[1]);
+      } catch (e) {
+        if (console && console.log) {
+          console.log(e);
+        }
+      } finally {
+        return false;
+      }      
     });
   });
   
